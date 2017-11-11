0573539d36492dfb625ad763a5e26056576312ce
vagrant <brittanyneal22@gmail.com>
2017-11-11 20:58:18 +0000

    working game

--- gg_pract.py
+++ gg_pract.py
@@ -5,4 +5,16 @@ print "Hello!"
 
 name = raw_input("Hey, what's your name? \n")
 
-rand_num = 
+rand_num = random.randint(1, 101)
+print rand_num
+count = 0
+while True:
+    guess = raw_input("Guess a number!")
+    count += 1
+    if int(guess) > rand_num:
+        print "That's too high"
+    elif int(guess) < rand_num:
+        print "that's too low"
+    elif int(guess) == rand_num:
+        print "You got it in {} tries!" .format(count)
+        break
